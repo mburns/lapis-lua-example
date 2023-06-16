386009cf3368fd3ef2c553ac5c5de27c5b0b10d9
Michael Burns <mburns@pop-os.localdomain>
2023-06-15 22:24:34 -0700

    fix views

--- app.lua
+++ app.lua
@@ -13,16 +13,13 @@ end)
 
 -- homepage
 app:get("homepage", "/", function()
-  return "Welcome, " .. tostring(self.current_user) .. ". " .. require("lapis.version")
+  return "Welcome, " .. require("lapis.version")
 end)
 
 -- testing
 app:match("/hello/:name", function(self)
   -- print(self.params.name)
   return { render = "hello" }
-end)
-app:get("/test", function(self)
-  return "I only render for GET requests"
 end)
 
 -- submit a new post
@@ -33,17 +30,40 @@ app:match("subreddits", "/subreddits", function(self) end)
 app:match("subreddit_search", "/subreddits/search", function(self) end)
 
 -- subreddit
-app:match("subreddit", "/r/:subreddit", function(self) end)
-app:match("subreddit", "/r/:subreddit/:sort", function(self) end)
-app:match("post_page", "/r/:subreddit[%w]/comments/:post_id[%w]/:title_stub[%w]", function(self) end)
-app:match("duplicates_page", "/r/:subreddit[%w]/duplicates/:post_id[%d]/:title_stub[%w]", function(self) end)
+app:match("subreddit", "/r/:subreddit", function(self)
+  -- print(self.params.subreddit)
+end)
+app:match("subreddit", "/r/:subreddit/*", function(self)
+  -- print(self.params.subreddit)
+  -- print(self.params.splat)
+end)
+app:match("post_page", "/r/:subreddit[%w]/comments/:post_id[%w]/:title_stub[%w]", function(self)
+  -- print(self.params.subreddit)
+  -- print(self.params.post_id)
+  -- print(self.params.title_stub)
+end)
+app:match("duplicates_page", "/r/:subreddit[%w]/duplicates/:post_id[%d]/:title_stub[%w]", function(self)
+  -- print(self.params.subreddit)
+  -- print(self.params.post_id)
+  -- print(self.params.title_stub)
+end)
 
 -- user
-app:match("user_profile", "/user/:username[%w]", function(self) end)
-app:match("user_comments", "/user/:username[%w]/comments", function(self) end)
-app:match("user_submitted", "/user/:username[%w]/submitted", function(self) end)
-app:match("user_settings", "/settings/*[%w]", function(self) end)
-app:match("user_preferences", "/prefs/*[%w]", function(self) end)
+app:match("user_profile", "/user/:username[%w]", function(self)
+  -- print(self.params.username)
+end)
+app:match("user_comments", "/user/:username[%w]/comments", function(self)
+  -- print(self.params.username)
+end)
+app:match("user_submitted", "/user/:username[%w]/submitted", function(self)
+  -- print(self.params.username)
+end)
+app:match("user_settings", "/settings/*[%w]", function(self)
+  -- print(self.params.splat)
+end)
+app:match("user_preferences", "/prefs/*[%w]", function(self)
+  -- print(self.params.splat)
+end)
 
 -- authentication
 app:match("login", "/login", function(self) end)
--- config.lua
+++ config.lua
@@ -7,5 +7,9 @@ config("development", {
   port = "8123",
   name = "lapis-example",
   session_name = "my_app_session",
-  secret = "A string witches also my s3cr3t!"
+  secret = "A string witches also my s3cr3t!",
+  sqlite = {
+    database = "site.sqlite"
+    -- open_falgs = ...
+  }
 })
