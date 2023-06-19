bc10882095b8253b9873e337136590f968e68423
Michael Burns <michael@useintegral.com>
2023-06-18 19:57:28 -0700

    lint

--- app/config.lua
+++ app/config.lua
@@ -5,16 +5,16 @@ local lua_path  = "./src/?.lua;./src/?/init.lua;./libs/?.lua;./libs/?/init.lua"
 local lua_cpath = ""
 
 config("development", {
-	port      = 80,
-	lua_path  = lua_path,
-	lua_cpath = lua_cpath,
-	server     = "nginx",
-	code_cache = "off",
-	num_workers = "1",
-	name        = "example",
-	session_name = "dev_app_session",
-	secret     = "change_me",
-	sqlite = {
-	  database = "/var/data/dev.sqlite"
-	}
+    port      = 80,
+    lua_path  = lua_path,
+    lua_cpath = lua_cpath,
+    server     = "nginx",
+    code_cache = "off",
+    num_workers = "1",
+    name        = "example",
+    session_name = "dev_app_session",
+    secret     = "change_me",
+    sqlite = {
+        database = "/var/data/dev.sqlite"
+    }
 })
--- app/src/actions/index.lua
+++ app/src/actions/index.lua
@@ -1,13 +1,12 @@
 local db = require "lapis.db"
 
 return {
-	before = function(self)
-		-- check if a table "example" exists
-		self.q = db.query("SELECT name FROM sqlite_master WHERE type='table' AND name=?", "example")
-	end,
+    before = function(self)
+        -- check if a table "example" exists
+        self.q = db.query("SELECT name FROM sqlite_master WHERE type='table' AND name=?", "example")
+    end,
 
-	GET = function(self)
-		return { render = "index" }
-	end
-
+    GET = function(self)
+        return { render = "index" }
+    end
 }
